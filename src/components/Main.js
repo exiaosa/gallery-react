d4930b211acc2ce5f283060f82b1098559890c7c
jenny <xinning.ren@gmail.com>
2017-05-24 11:14:20 +1200

    set three sections to display images by random

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,6 +2,9 @@
 require('styles/App.scss');
 
 import React from 'react';
+//import {findDOMNode} from 'react-dom';
+//var ReactDOM = require('react-dom');
+import {findDOMNode} from 'react-dom';
 
 //get all the image data
 let imageDatas = require('../data/image.json');
@@ -10,7 +13,7 @@ let imageDatas = require('../data/image.json');
 function getImageURL(imageDatasArr) {
     for (var i = 0, j = imageDatasArr.length; i < j; i++) {
         var singleImageData = imageDatasArr[i];
-
+		//require('../XXX'+fileNmae) can get the image url
         singleImageData.imageURL = require('../images/' + singleImageData.fileName);
 
         imageDatasArr[i] = singleImageData;
@@ -19,14 +22,30 @@ function getImageURL(imageDatasArr) {
     return imageDatasArr;
 }
 
+//convert all the image name into image url
 imageDatas = getImageURL(imageDatas);
+
+
+//get the random value in a given range
+function getRangeRandom(low, high) {
+    return Math.ceil(Math.random() * (high - low) + low);
+}
 
 
 var ImgFigure = React.createClass({
 	render:function(){
+		var styleObj = {};
+
+        //如果props属性中指定了这张图片的位置，则使用
+		
+        if (this.props.arrange.pos) {
+            styleObj = this.props.arrange.pos;
+        }
+		
+        
 		return(
-			<figure className="img-figure">
-				<img src={this.props.data.imageURL} alt={this.props.data.title}/>
+			<figure className="img-figure" style={styleObj}>
+				<img src={this.props.data.imageURL} alt={this.props.data.title} />
 				<figcaption>
 					<h2 className="img-title">{this.props.data.title}</h2>
 				</figcaption>
@@ -36,18 +55,176 @@ var ImgFigure = React.createClass({
 	}
 });
 
-class AppComponent extends React.Component {
+
+
+
+
+
+var AppComponent = React.createClass( {
+  Constant:{
+	centerPos:{
+	  left:0,
+	  right:0
+	},
+	//horizontal range
+	hPosRange:{
+	  leftSecX: [0,0],
+	  rightSecX: [0,0],
+	  y: [0,0]
+	},
+	//vertical range
+	vPosRange:{
+	  x: [0,0],
+	  topY: [0,0]
+	}
+  },
+  
+  /*
+   *rearrange all the images
+   *@param centerIndex: point out which image is in center
+   */
+  rearrange: function(centerIndex){
+    var imgsArrangeArr = this.state.imgsArrangeArr,
+        Constant = this.Constant,
+        centerPos = Constant.centerPos,
+        hPosRange = Constant.hPosRange,
+        vPosRange = Constant.vPosRange,
+        hPosRangeLeftSecX = hPosRange.leftSecX,
+        hPosRangeRightSecX = hPosRange.rightSecX,
+        hPosRangeY = hPosRange.y,
+        vPosRangeTopY = vPosRange.topY,
+        vPosRangeX = vPosRange.x,
+		
+		imgsArrangeTopArr = [],
+		
+        topImgNum = Math.floor(Math.random() * 2),    /* 0-1 */
+		
+        topImgSpliceIndex = 0,  /*the splice index for the image array which is shown at top*/
+		
+		imgsArrangeCenterArr = imgsArrangeArr.splice(centerIndex, 1);
+		
+		/* align the centerIndex image, no rotate */
+        imgsArrangeCenterArr[0].pos = centerPos;
+		
+		/* get the image status in the top section */
+        topImgSpliceIndex = Math.ceil(Math.random() * (imgsArrangeArr.length - topImgNum));
+        imgsArrangeTopArr = imgsArrangeArr.splice(topImgSpliceIndex, topImgNum);
+		
+		/* arrange images at the TOP section */
+        imgsArrangeTopArr.forEach(function (value, index) {
+            imgsArrangeTopArr[index]= {
+              pos:{
+                  top: getRangeRandom(vPosRangeTopY[0], vPosRangeTopY[1]),
+                  left: getRangeRandom(vPosRangeX[0], vPosRangeX[1])
+              }
+            };
+        });
+		
+		/* images in LEFT & RIGHT sections */
+        for (var i = 0, j = imgsArrangeArr.length, k = j / 2; i < j; i++) {
+            var hPosRangeLORX = null;
+
+            /* 前半部分布局左边， 右半部分布局右边 */
+            if (i < k) {
+                hPosRangeLORX = hPosRangeLeftSecX;
+            } else {
+                hPosRangeLORX = hPosRangeRightSecX;
+            }
+
+            imgsArrangeArr[i].pos={
+                top: getRangeRandom(hPosRangeY[0],hPosRangeY[1]),
+                left: getRangeRandom(hPosRangeLORX[0],hPosRangeLORX[1])
+            };
+			
+        }
+		
+		if (imgsArrangeTopArr && imgsArrangeTopArr[0]) {
+            imgsArrangeArr.splice(topImgSpliceIndex, 0, imgsArrangeTopArr[0]);
+        }
+
+        imgsArrangeArr.splice(centerIndex, 0, imgsArrangeCenterArr[0]);
+
+        this.setState({
+            imgsArrangeArr: imgsArrangeArr
+        });
+  },
+  
+  getInitialState:function(){
+	return{
+	  imgsArrangeArr:[
+		{
+			/*
+			pos:{
+				left:'0',
+				top:'0'
+			}
+			*/
+		}
+	  ]
+	};
+  },
+  
+  /* calculate each image's positions range */
+  componentDidMount:function(){
+  
+    /* get stage size */
+    var stageDOM = findDOMNode(this.refs.stage),
+	    stageW = stageDOM.scrollWidth,
+		stageH = stageDOM.scrollHeight,
+		halfStageW = Math.ceil(stageW/2),
+		halfStageH = Math.ceil(stageH/2);
+	
+	/* get imageFigure size */
+    var imgFigureDOM = findDOMNode(this.refs.imgFigure0),
+	    imgW = imgFigureDOM.scrollWidth,
+		imgH = imgFigureDOM.scrollHeight,
+		halfImgW = Math.ceil(imgW/2),
+        halfImgH = Math.ceil(imgH/2);
+	
+    /* calculate centre-image position	*/
+	this.Constant.centerPos={
+		left:halfStageW - halfImgW,
+		top: halfStageH - halfImgH
+	};
+	
+	/* calculate the image range in the left section & right section*/
+	this.Constant.hPosRange.leftSecX[0] = -halfImgW;
+	this.Constant.hPosRange.leftSecX[1] = halfStageW - halfImgW * 3;
+	this.Constant.hPosRange.rightSecX[0] = halfStageW + halfImgW;
+    this.Constant.hPosRange.rightSecX[1] = stageW - halfImgW;
+    this.Constant.hPosRange.y[0] = -halfImgH;
+    this.Constant.hPosRange.y[1] = stageH - halfImgH;
+	
+	/* calculate the image range in the top section */
+	this.Constant.vPosRange.topY[0] = -halfImgH;
+    this.Constant.vPosRange.topY[1] = halfStageH - halfImgH * 3;
+    this.Constant.vPosRange.x[0] = halfStageW - imgW;
+    this.Constant.vPosRange.x[1] = halfStageW;
+	
+	this.rearrange(0);
+  },
+
   render(){
   
     var controllerUnits = [],
 	    imgFigures = [];
   
-	imageDatas.forEach(function(value){
-		imgFigures.push(<ImgFigure data={value} />);
-	});
+	imageDatas.forEach(function(value,index){
+		
+		if (!this.state.imgsArrangeArr[index]) {
+            this.state.imgsArrangeArr[index] = {
+			    pos:{
+					top: 0,
+					left: 0
+				}
+            };
+        }
+     
+		imgFigures.push(<ImgFigure key={index} data={value} ref={'imgFigure'+index} arrange={this.state.imgsArrangeArr[index]} />);
+	}.bind(this));
 	
     return (
-      <section className="stage">
+      <section className="stage" ref="stage">
         <section className="img-src">
 			{imgFigures}
 		</section>
@@ -57,7 +234,8 @@ class AppComponent extends React.Component {
       </section>
     );
   }
-}
+})
+
 
 AppComponent.defaultProps = {
 };
